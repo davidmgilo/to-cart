148ed69242c7b0fba5577a12acd1509f501b5446
David Martínez Gilo <davidmgilo@gmail.com>
2022-12-11 18:27:34 +0100

    add fontawesome & base layout

--- src/App.vue
+++ src/App.vue
@@ -4,88 +4,102 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <header class="navbar">
+    <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <!-- <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"> -->
+      <span class="icon-text has-text-primary">
+        <span class="icon is-large">
+          <span class="fa-stack">
+            <font-awesome-icon class="fa-stack-2x" transform="shrink-6 left-8" icon="fa-solid fa-angles-right"/>
+            <font-awesome-icon class="fa-stack-2x" icon="fa-solid fa-cart-shopping" /> 
+          </span>
+        </span>
+        <span class="is-size-3 pt-2">To-cart</span>
+      </span>
+      
+    </a>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+    <!-- <HelloWorld msg="You did it!" /> -->
+ 
+    <!--<div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+    </div> -->
   </header>
 
-  <RouterView />
+  <div id="content">
+    <aside class="menu">
+      <p class="menu-label">
+        General
+      </p>
+      <ul class="menu-list">
+        <li><a>Dashboard</a></li>
+        <li><a>Customers</a></li>
+      </ul>
+      <p class="menu-label">
+        Administration
+      </p>
+      <ul class="menu-list">
+        <li><a>Team Settings</a></li>
+        <li>
+          <a class="is-active">Manage Your Team</a>
+          <ul>
+            <li><a>Members</a></li>
+            <li><a>Plugins</a></li>
+            <li><a>Add a member</a></li>
+          </ul>
+        </li>
+        <li><a>Invitations</a></li>
+        <li><a>Cloud Storage Environment Settings</a></li>
+        <li><a>Authentication</a></li>
+      </ul>
+      <p class="menu-label">
+        Transactions
+      </p>
+      <ul class="menu-list">
+        <li><a>Payments</a></li>
+        <li><a>Transfers</a></li>
+        <li><a>Balance</a></li>
+      </ul>
+    </aside>
+  
+    <RouterView id="main" />
+  </div>
+
 </template>
 
 <style scoped>
+
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  height: 30px;
+  flex-basis: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+aside {
+  overflow-y: auto; 
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+#content{
+  display: flex;
+  height: 100%;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+#main{
+  overflow-y: auto; 
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+@media (max-width: 1024px) {
+  #content{
+    flex-direction: column;
   }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,17 @@ import "../node_modules/bulma/css/bulma.css";
 
 import "./assets/main.css";
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCartShopping);
+library.add(faAnglesRight);
+
 const app = createApp(App);
 
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
 
 app.mount("#app");
